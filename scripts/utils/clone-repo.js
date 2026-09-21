6933e56747d79d85b8f177d385a6296b7e1a0ae3
paulAlexSerban <paul.alex.serban@gmail.com>
2026-09-21 18:37:22 +0300

    fix: vars

--- scripts/utils/clone-repo.js
+++ scripts/utils/clone-repo.js
@@ -4,7 +4,6 @@ const path = require("path");
 const fs = require("fs");
 const dotendv = require("dotenv");
 
-const CONTENT_REPO_DIRECTORY = "frontend/portfolio-website/content/repo";
 
 dotendv.config();
 const { CONTENT_REPO_TOKEN, GITHUB_TOKEN, CONTENT_REPO_GIT_URL, CONTENT_REPO_DIRECTORY } =
